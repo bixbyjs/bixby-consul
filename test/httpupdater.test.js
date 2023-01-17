6cadcdd316ce490bbd5ae7f69d221282cde5fdb3
Jared Hanson <jaredhanson@gmail.com>
2023-01-17 06:36:28 -0800

    Clean up service registration.

--- test/httpupdater.test.js
+++ test/httpupdater.test.js
@@ -11,19 +11,24 @@ describe('HTTPUpdater', function() {
   describe('#register', function() {
     var client = new Updater(_client);
     
-    it.skip('should register record', function(done) {
+    it('should register record', function(done) {
       _client.agent = {};
       _client.agent.service = {};
       _client.agent.service.register = sinon.stub().yieldsAsync(null);
       
       var rr = {
-        name: 'example.local'
+        name: 'example.local',
+        port: 833
       };
-      client.register('hashicorp.node.dc1.consul', 'SRV', rr, function(err) {
-        expect(_client.agent.service.register.getCall(0).args[0]).to.deep.equal({
-          name: 'boop',
+      client.register('_beep._tcp.consul', 'SRV', rr, function(err) {
+        var record = _client.agent.service.register.getCall(0).args[0];
+        expect(record.id).to.be.a('string');
+        expect(record.id).to.have.length(36)
+        delete record.id;
+        expect(record).to.deep.equal({
+          name: 'beep',
           address: 'example.local',
-          port: 800
+          port: 833
         });
         
         expect(err).to.be.null;
