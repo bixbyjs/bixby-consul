1b724cb0f4fe69b738a445d47a676f9c3aba14d3
Jared Hanson <jaredhanson@gmail.com>
2024-09-02 15:31:00 -0700

    Clarify tests.

--- test/httpresolver.test.js
+++ test/httpresolver.test.js
@@ -85,22 +85,6 @@ describe('HTTPResolver', function() {
       });
     }); // should resolve CNAME record of external node in datacenter
     
-    it('should resolve SRV record of service using agent address', function(done) {
-      _client.catalog = {};
-      _client.catalog.service = {};
-      _client.catalog.service.nodes = sinon.stub().yieldsAsync(null, JSON.parse(fs.readFileSync('test/data/http/v1/catalog/service/beep.json', 'utf8')));
-      
-      client.resolve('_beep._tcp.consul', 'SRV', function(err, addresses) {
-        expect(_client.catalog.service.nodes).to.be.calledOnceWith('beep');
-        
-        expect(err).to.be.null;
-        expect(addresses).to.deep.equal([
-          { name: 'node1.node.dc1.consul', port: 833 }
-        ]);
-        done();
-      });
-    }); // should resolve SRV record of service using agent address
-    
     it('should resolve SRV record of service', function(done) {
       _client.catalog = {};
       _client.catalog.service = {};
@@ -133,6 +117,22 @@ describe('HTTPResolver', function() {
         done();
       });
     }); // should resolve SRV record of service running on multiple ports
+    
+    it('should resolve SRV record of service using agent address', function(done) {
+      _client.catalog = {};
+      _client.catalog.service = {};
+      _client.catalog.service.nodes = sinon.stub().yieldsAsync(null, JSON.parse(fs.readFileSync('test/data/http/v1/catalog/service/beep.json', 'utf8')));
+      
+      client.resolve('_beep._tcp.consul', 'SRV', function(err, addresses) {
+        expect(_client.catalog.service.nodes).to.be.calledOnceWith('beep');
+        
+        expect(err).to.be.null;
+        expect(addresses).to.deep.equal([
+          { name: 'node1.node.dc1.consul', port: 833 }
+        ]);
+        done();
+      });
+    }); // should resolve SRV record of service using agent address
     
     it('should resolve SRV record of external service', function(done) {
       _client.catalog = {};
