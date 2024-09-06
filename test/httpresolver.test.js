b1fa765f71b5746e7d732d0ee86a3cc912015a12
Jared Hanson <jaredhanson@gmail.com>
2024-09-06 10:19:16 -0700

    Add test case for configured domain.

--- test/httpresolver.test.js
+++ test/httpresolver.test.js
@@ -134,6 +134,23 @@ describe('HTTPResolver', function() {
       });
     }); // should resolve SRV record of service using agent address
     
+    it('should resolve SRV record of service using agent address with configured domain', function(done) {
+      _client.catalog = {};
+      _client.catalog.service = {};
+      _client.catalog.service.nodes = sinon.stub().yieldsAsync(null, JSON.parse(fs.readFileSync('test/data/http/v1/catalog/service/beep.json', 'utf8')));
+      
+      var client = new Resolver(_client, 'internal');
+      client.resolve('_beep._tcp.consul', 'SRV', function(err, addresses) {
+        expect(_client.catalog.service.nodes).to.be.calledOnceWith('beep');
+        
+        expect(err).to.be.null;
+        expect(addresses).to.deep.equal([
+          { name: 'node1.node.dc1.internal', port: 833 }
+        ]);
+        done();
+      });
+    }); // should resolve SRV record of service using agent address with configured domain
+    
     it('should resolve SRV record of external service', function(done) {
       _client.catalog = {};
       _client.catalog.service = {};
